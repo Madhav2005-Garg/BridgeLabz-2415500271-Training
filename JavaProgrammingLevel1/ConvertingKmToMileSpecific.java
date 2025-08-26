7589228eabd103ca3ebb34aabee2d67a377329f1
Madhav2005-Garg <madhavgarg545@gmail.com>
2025-08-26 20:06:16 +0530

    Delete JavaProgrammingLevel1 directory

--- JavaProgrammingLevel1/ConvertingKmToMileSpecific.java
+++
@@ -1,8 +0,0 @@
-public class ConvertingKmToMileSpecific {
-    public static void main(String[] args) {
-        double kilometers = 10.8;
-        double miles = kilometers * 0.621371;
-
-        System.out.println(kilometers + " km is " + miles + " miles.");
-    }
-}
